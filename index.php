5f8007823de549f8f1852f593ee9216426f42f2a
codedevloper13 <54003879+codedevloper13@users.noreply.github.com>
2020-08-05 22:55:36 +0530

    hello

--- index.php
+++ index.php
@@ -22,13 +22,13 @@ get_header(); ?>
                         <figure><?php the_post_thumbnail('medium-large') ?></figure>
                     </div>
                 </div>
-                <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                <h4 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
                 <div class="metabox">
                     <p>Posted by <?php the_author_posts_link(); ?> on <?php the_time('n.j.y'); ?> in <?php echo get_the_category_list(', '); ?></p>
                 </div>
 
                 <div class="generic-content">
-                    <?php the_excerpt(); ?>
+                    <p><?php  echo wp_trim_words(get_the_content(),55); ?></p>
                     <p><a class="btn btn--blue" href="<?php the_permalink(); ?>">Continue reading &raquo;</a></p>
                 </div>
 
